681f82aa6c50afb1d2fda0147112d8658f5b8c78
Shree <khape.jai@gmail.com>
2026-08-24 22:19:37 +0530

    Learn NumPy array operations

---
+++ Basic/np.array.oper.py
@@ -0,0 +1,14 @@
+import numpy as np 
+
+a = np.array([10, 20, 30])
+b = np.array([1, 2, 3])
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+prices = np.array([100, 200, 300])
+
+print(prices * 2)
+print(prices + 50)
