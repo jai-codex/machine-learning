4dec7a54f1ccf57ac044e8563dc30641f5c6f298
Shree <khape.jai@gmail.com>
2026-08-24 22:15:07 +0530

    Learn NumPy array indexing

---
+++ Basic/np.indexing.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+numbers = np.array([5, 10, 15, 20, 25])
+
+print(numbers[2])
+print(numbers[4])
+
+data = np.array([
+    [10, 20, 30],
+    [40, 50, 60]
+])
+
+print(data.shape)
+print(data[1][1])
