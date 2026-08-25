228ab6451a2fc6427918e7a544b464dc1b0d48bc
Shree <khape.jai@gmail.com>
2026-08-25 21:07:29 +0530

    Learn NumPy zeros ones and random arrays

---
+++ Basic/np.0.1.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+a = np.zeros(5)
+print(a)
+b = np.ones(5)
+print(b)
+
+a = np.zeros((2 , 3))
+print(a)
+b = np.ones((2, 3))
+print(b)
+
+a = np.random.rand(5)
+print(a)
+
+a = np.random.randint(1, 10, 5)
+print(a)
+
+a = np.random.rand(2, 3)
+print(a)
+
+x = np.zeros((3, 3))
+print(x)
---
+++ Basic/np.arange.py
@@ -0,0 +1,9 @@
+import  numpy as np
+
+numbers = np.arange(1, 6)
+
+print(numbers)
+
+numbers = np.arange(0, 10 ,2)
+
+print(numbers)
