701d11d094ce859392787574dc93fdecba511b61
Shree <khape.jai@gmail.com>
2026-08-25 20:37:41 +0530

    Learn NumPy statistical functions

---
+++ Basic/np.static.oper.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+marks = np.array([70, 80, 90, 60, 85])
+
+print("Average Value:", np.mean(marks))
+print("Mini Value   :", np.min(marks))
+print("Max Value    :", np.max(marks))
+
+print("--------------------")
+
+data = np.array([10, 20, 30, 40, 50])
+
+print("Average Value:", np.mean(data))
+print("Mini Value   :", np.min(data))
+print("Max Value    :", np.max(data))
