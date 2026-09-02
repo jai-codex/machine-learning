dfa4a5d3225b5534d6e2bcb4a299804897ab894b
Shree <khape.jai@gmail.com>
2026-09-02 18:30:50 +0530

    Learn basic vector operations

---
+++ Linear_Algebra/vector_operations.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+vector1 = np.array([10, 20, 30])
+vector2 = np.array([40, 50, 60])
+
+print("Addition:", vector2 + vector1)
+
+print("Substraction:", vector1 - vector2)
+
+print("Multiply by 2:",vector1 * 2)
