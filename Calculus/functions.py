1cc474a108ea402e9a8c7bd216cc0ff7e01e31ba
Shree <khape.jai@gmail.com>
2026-09-03 18:20:49 +0530

    Learn functions for machine learning calculus

---
+++ Calculus/functions.py
@@ -0,0 +1,14 @@
+def square(x):
+    return x*x
+
+number = 5
+
+result = square(number)
+print("Input:", number)
+print("Output:", result)
+
+def calculate(hours):
+    marks = hours * 10
+    return marks
+
+print("Marks:", calculate(5))    
