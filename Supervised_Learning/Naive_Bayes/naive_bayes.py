1ce8d5fab62b740c9bfb154204d3b80890bb1602
Shree <khape.jai@gmail.com>
2026-09-10 18:17:38 +0530

    Learn Naive Bayes classification

---
+++ Supervised_Learning/Naive_Bayes/naive_bayes.py
@@ -0,0 +1,18 @@
+from sklearn.naive_bayes import GaussianNB
+
+# Feature: Study Hours
+X = [[1], [2], [3], [6], [7], [8]]
+
+# Target: 0 = Fail, 1 = Pass
+y = [0, 0, 0, 1, 1, 1]
+
+# Create the model
+model = GaussianNB()
+
+# Train the model
+model.fit(X, y)
+
+# Make a prediction
+prediction = model.predict([[5]])
+
+print("Prediction:", prediction[0])
---
+++ Supervised_Learning/Naive_Bayes/train_test_naive_bayes.py
@@ -0,0 +1,26 @@
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+X = [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]]
+
+y = [0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+model = GaussianNB()
+
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, predictions)
+
+print("Predictions:", predictions)
+print("Actual Values:", y_test)
+print("Accuracy:", accuracy)
