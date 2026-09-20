afc54722da88f9ede3a33ca3c11562d9bbe619f5
Shree <khape.jai@gmail.com>
2026-09-20 12:05:00 +0530

    Train random forest house price model

---
+++ Supervised_Learning/project/California_Housing/25_house_price_random_forest.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+data = fetch_california_housing()
+df = pd.DataFrame(data.data, columns=data.feature_names)
+df["MedHouseVal"] = data.target
+
+X = df.drop("MedHouseVal", axis=1)
+y = df["MedHouseVal"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+model = RandomForestRegressor(
+    random_state=42,
+    n_estimators=100,
+    n_jobs=-1
+)
+
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+
+mae = mean_absolute_error(y_test, predictions)
+mse = mean_squared_error(y_test, predictions)
+rmse = mse*mse
+r2= r2_score(y_test, predictions)
+
+print("MAE:", mae)
+print("MSE:", mse)
+print("RMSE:", rmse)
+print("R2:", r2)
